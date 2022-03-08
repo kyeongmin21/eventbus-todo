ada15ba1d6747f96215eeac522033153fc29c049
kim kyeong min <kyeongmin21@naver.com>
2022-03-08 21:26:56 +0900

    페이지 추가

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,42 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/model',
+    name: 'Model',
+    component: () => import(/* webpackChunkName: "model" */ '../views/Model.vue')
+  },
+  {
+    path: '/if',
+    name: 'If',
+    component: () => import(/* webpackChunkName: "if" */ '../views/If.vue')
+  },
+  {
+    path: '/class',
+    name: 'Class',
+    component: () => import(/* webpackChunkName: "class" */ '../views/Class.vue')
+  },
+  {
+    path: '/compo',
+    name: 'Compo',
+    component: () => import(/* webpackChunkName: "compo" */ '../views/Compo.vue')
+  },
+  {
+    path: '/parent',
+    name: 'Parent',
+    component: () => import(/* webpackChunkName: "parent" */ '../views/Parent.vue')
+  },
+  {
+    path: '/page',
+    name: 'Page',
+    component: () => import(/* webpackChunkName: "page" */ '../views/PageMove.vue')
+  },
 ]
 
 const router = new VueRouter({
